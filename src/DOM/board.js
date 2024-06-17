debe95c0558a311ccb2ccde345bc7edf7588c8a2
Ukam <ukam@Xubuntu.myguest.virtualbox.org>
2024-06-17 21:54:04 +0000

    Add files

--- src/DOM/board.js
+++ src/DOM/board.js
@@ -1,8 +1,6 @@
 
 //import './style.css';
 
-/* const board1 = document.querySelector('.board1');
-const board2 = document.querySelector('.board2'); */
 
 import { Ship, Gameboard, Player } from "../battleShip";
 
@@ -31,7 +29,7 @@ function gridStructure(board,player, num =10) {
             const contents = document.createElement('div');
             contents.classList.add(`contents${player}`);
             contents.classList.add('contents');
-            contents.id = `${j},${i}`;
+            contents.id = `${j},${i},${player}`;
 
             const span = document.createElement('span');
             span.append(arr[j][i]);
@@ -47,38 +45,63 @@ function gridStructure(board,player, num =10) {
     }
 
     const contents = document.querySelectorAll(`.contents${player}`);
-    const cont1 = document.querySelectorAll('.content1 .contents1');
-    console.log(cont1.textContent);
-    console.log(contents.textContent);
 
-    if(player == 2){ 
-        contents.forEach(contents => {
-            contents.addEventListener('click', (e) => {
+    const contents2 = document.querySelectorAll('.contents2.contents');
+    const cont = document.querySelectorAll('.contents1.contents');
 
-                let ind = e.target.id;
-                if(contents.textContent === "null"){
-                    contents.style.backgroundColor = 'blue';
-                    AllShips.receiveAttack(ind[0],ind[2]);
-                    console.log(arr);
-                }else if(contents.textContent !== "null"){
-                    contents.style.backgroundColor = 'red';
-                    AllShips.receiveAttack(ind[0],ind[2]);// Hit 
-                    
-                    if( AllShips.allSank()== "All ships have been sunk" ){
-                        console.log("Ships sank");
-                        return;
-                    }
-                    console.log(arr);
-                }  
+    ////if(player == 2){ }
+    contents2.forEach(contents => {
+        contents.addEventListener('click', (e) => {
 
+            let ind = e.target.id;
+            if(contents.textContent === "null"){
+                contents.style.backgroundColor = 'blue';
+                AllShips.receiveAttack(ind[0],ind[2]);
+                //console.log(arr);
+            }else if(contents.textContent !== "null"){
+                contents.style.backgroundColor = 'red';
+                AllShips.receiveAttack(ind[0],ind[2]);// Hit 
                 
-                //console.log(cont1.textContent);
-                //compPlay(arr, cont1);
+                if( AllShips.allSank()== "All ships have been sunk" ){
+                    console.log("Ships sank, player 1 wins");
+                    return;
+                }
+                //console.log(arr);
+            }  
+
+            
+            let indComp = getComputerChoice(arr, "X", "M"); 
+            
+            console.log(indComp[0],indComp[1],indComp[2]);
+
+            const cont1 = document.getElementById(`${indComp[0]},${indComp[1]},1`);
+            console.log(cont1.textContent);
+            
+            if(cont1.textContent === "X" || cont1.textContent === "M"){
+                indComp = getComputerChoice(arr); 
+            }
+
+            if(cont1.textContent === "null"){
+                cont1.style.backgroundColor = 'blue';
+                AllShips.receiveAttack(indComp[0],indComp[2]);
+                console.log(arr);
+            }else if(cont1.textContent !== "null"){
+                cont1.style.backgroundColor = 'red';
+                AllShips.receiveAttack(indComp[0],indComp[2]);// Hit 
                 
-            });
+                if( AllShips.allSank()== "All ships have been sunk" ){
+                    console.log("Ships sank, player2 wins");
+                    return;
+                }
+                console.log(arr);
+            }  
+            
+
+            
         });
-    }
-   
+    });
+    
+ 
     if(player == 1){
         contents.forEach(contents => {
             if(contents.textContent !== "null"){
@@ -90,11 +113,23 @@ function gridStructure(board,player, num =10) {
 
 }
 
-function getComputerChoice(choices) {
+function getComputerChoice(choices,a,b) {
     //const choices = ["Rock","Paper","Scissors"];
-    const randomChosen = choices[Math.floor(Math.random()*choices.length)];
-    const randomNumber = randomChosen[Math.floor(Math.random()*randomChosen.length)]
-    return randomNumber;
+    /* const randomChosen = choices[Math.floor(Math.random()*choices.length)];
+    const randomNumber = randomChosen[Math.floor(Math.random()*randomChosen.length)] */
+    let randomChosen = Math.floor(Math.random()*choices.length);
+    let ran1d = choices[randomChosen];
+    let randomNumber = Math.floor(Math.random()*ran1d.length);
+    let ran2d = ran1d[Math.floor(Math.random()*ran1d.length)];
+
+    while(ran2d == a || ran2d == b){
+        randomChosen = Math.floor(Math.random()*choices.length);
+        ran1d = choices[randomChosen];
+        randomNumber = Math.floor(Math.random()*ran1d.length);
+        ran2d = ran1d[Math.floor(Math.random()*ran1d.length)];
+    }
+
+    return [randomChosen, randomNumber,ran2d]; //, randomNumber, ran1d, ran2d;
 }
 
 function compPlay(arr, contents){
